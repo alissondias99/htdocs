2c82f73efa8dbc151143a68289e7446c735694af
alissondias99 <dias95357@gmail.com>
2023-10-18 11:49:14 -0300

    Teste

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- Aula 308 e 309. Tags do php-->
+    <?php 
+        echo 'Utilizando a tag padrão'; 
+    ?>
+    <br>
+
+    <?= 'Utilizando a tag de impressão' ?>
+    <br>
+
+    <? echo 'Utilizando a tag curta' ?>
+    <br>
+    <br>
+    <!--Aula 310. Echo e print -->
+    <?php 
+        echo 'Saida de dados com echo <br>';
+    
+        echo 'Saida de dados com print'; 
+    ?>
+
+    <!--Aula 312. Comentários-->
+    <?php 
+        // Coment de um linha
+        # Coment de um linha no estilo shell
+        /*Coment de varias linhas
+        Todos os 3 só funcionam dentro da tag php*/ 
+    ?>
+</body>
+</html>
